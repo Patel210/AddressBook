69cdd60f5f42a025e581c7018d5a76d26011cee8
Patel210 <pranavpatel2101997@gmail.com>
2020-10-06 22:24:22 +0530

    Added: remove contact method to remove contact from address book

--- src/main/java/com/capgemini/addressbook/AddressBookMain.java
+++ src/main/java/com/capgemini/addressbook/AddressBookMain.java
@@ -2,7 +2,6 @@ package com.capgemini.addressbook;
 
 import java.util.Scanner;
 
-
 public class AddressBookMain {
 
 	private Contact[] addressBook;
@@ -31,28 +30,28 @@ public class AddressBookMain {
 		System.out.println("Contact added successfully!");
 		noOfContacts++;
 	}
-	
-	
+
 	/**
 	 * To get the contact from address book using first and last name
 	 */
 	public Contact getContact(String firstName, String lastName) {
 		Contact contact = null;
-		for (int i = 0 ; i < noOfContacts; i++) {
-			if(addressBook[i].getFirstName().equalsIgnoreCase(firstName) && addressBook[i].getLastName().equalsIgnoreCase(lastName)) {
+		for (int i = 0; i < noOfContacts; i++) {
+			if (addressBook[i].getFirstName().equalsIgnoreCase(firstName)
+					&& addressBook[i].getLastName().equalsIgnoreCase(lastName)) {
 				contact = addressBook[i];
 				break;
 			}
 		}
 		return contact;
 	}
-	
+
 	/**
 	 * To edit contact in the address book
 	 */
 	public void editContact(String firstName, String lastName) {
 		Contact contact = getContact(firstName, lastName);
-		if(contact!=null) {
+		if (contact != null) {
 			System.out.println("Enter the details again to update the contact \nfirst name:");
 			contact.setFirstName(SC.next());
 			System.out.println("last name:");
@@ -61,7 +60,7 @@ public class AddressBookMain {
 			SC.nextLine();
 			contact.setAddress(SC.nextLine());
 			System.out.println("City: ");
-			contact.setCity(SC.next());
+			contact.setCity(SC.nextLine());
 			System.out.println("State: ");
 			contact.setState(SC.next());
 			System.out.println("Zip: ");
@@ -72,42 +71,46 @@ public class AddressBookMain {
 			contact.setEmail(SC.next());
 			System.out.println("Contact Updated Successfully!");
 		}
-		
+
 		else {
 			System.out.println("NO such contact in the address book!");
 		}
 	}
 
-	public static void main(String[] args) {
-		AddressBookMain addressBookMain = new AddressBookMain();
+	/**
+	 * To remove contact from the address book
+	 */
+	public void removeContact(String firstName, String lastName) {
 
-		System.out.println("Enter the number of contacts you add into the address book (Max limit is 50): ");
-		int numberOfContacts = SC.nextInt();
-		for (int i = 0; i < numberOfContacts; i++) {
-			System.out.println("Enter the contact details \nfirst name:");
-			String firstName = SC.next();
-			System.out.println("last name:");
-			String lastName = SC.next();
-			System.out.println("Address: ");
-			SC.nextLine();
-			String address = SC.nextLine();
-			System.out.println("City: ");
-			String city = SC.nextLine();
-			System.out.println("State: ");
-			String state = SC.nextLine();
-			System.out.println("Zip: ");
-			long zip = SC.nextLong();
-			System.out.println("Phone Number: ");
-			long phoneNumber = SC.nextLong();
-			System.out.println("Email Address: ");
-			String email = SC.next();
-			addressBookMain.addContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
-			
-			System.out.println("Enter first name and last name to edit the contact");
-			addressBookMain.editContact(SC.next(), SC.next());
-			
+		Contact contact = getContact(firstName, lastName);
+		System.out.println(contact);
 
+		if (contact != null) {
+			Contact[] copyOfAddressBook = new Contact[addressBook.length];
+			for (int i = 0; i < addressBook.length; i++) {
+				if (addressBook[i] != (contact)) {
+					copyOfAddressBook[i] = addressBook[i];
+				}
+			}
+
+			addressBook = copyOfAddressBook;
+			System.out.println("Contact removed from the address book successfully");
+		} else {
+			System.out.println("Sorry, no such contact exist with that name!");
 		}
+	}
+
+	public static void main(String[] args) {
+
+		AddressBookMain addressBookMain = new AddressBookMain();
+		addressBookMain.addContact("Tom", "Harry", "14, Brooklyn Heights", "New York City", "New York",
+				"tom.harry@yahoo.com", 11208, 1002359753);
+		addressBookMain.addContact("Peter", "Clark", "18, Brooklyn Heights", "New York City", "New York",
+				"peter.clark@yahoo.com", 11208, 1002359896);
+
+		addressBookMain.editContact("Tom", "Harry");
+		addressBookMain.removeContact("Peter", "Clark");
 
 	}
+
 }

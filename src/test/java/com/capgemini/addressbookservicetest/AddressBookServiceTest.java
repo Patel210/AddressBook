f6ba4594bddc6774a713dfdf147c80d67065a97b
Patel210 <pranavpatel2101997@gmail.com>
2020-10-29 20:40:54 +0530

    Added test case to update the contact's phone number and to check whether the given contact is in sync with DB

--- src/test/java/com/capgemini/addressbookservicetest/AddressBookServiceTest.java
+++ src/test/java/com/capgemini/addressbookservicetest/AddressBookServiceTest.java
@@ -13,6 +13,15 @@ public class AddressBookServiceTest {
 		AddressBookService addressBookService = new AddressBookService();
 		int enteries = addressBookService.readAddressBook();
 		assertEquals(7, enteries);
+	} 
+	
+	@Test
+	public void givenNewPhoneNumberForAContact_WhenUpdated_ShouldBeUpdatedInTheDB() {
+		AddressBookService addressBookService = new AddressBookService();
+		addressBookService.readAddressBook();
+		addressBookService.updateContactPhoneNumber(1, 9876543210L);
+		boolean result = addressBookService.isContactInSyncWithDB(2);
+		assertTrue(result);
 	}
 
 }
